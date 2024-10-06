48d9d048efc3a87e0c512683635ec3b7f80b5970
M.Hassan Gulzar <hassangulzar121@gmail.com>
2024-10-06 22:03:35 +0500

    change the name

--- src/ThreeDModel/index.jsx
+++ src/ThreeDModel/index.jsx
@@ -163,7 +163,7 @@ export default function ThreeDModel() {
     setIsLoading(true);
 
     try {
-      const response = await fetch("http://127.0.0.1:5000//chatbot", {
+      const response = await fetch("http://127.0.0.1:5000/chatbot", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
